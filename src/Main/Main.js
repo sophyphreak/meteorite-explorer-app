a126a6800094955b873dfb80c7e59801b4c3aaab
Andrew Horn <arhorn@smcm.edu>
2019-05-11 14:07:05 -0500

    stripped out unneeded boilerplate, added app skeleton

---
+++ src/Main/Main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Main = () => <p>this is the main part</p>;
+
+export default Main;
---
+++ src/TitleBar/TitleBar.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const TitleBar = () => <h1>Meteorite Explorer App</h1>;
+
+export default TitleBar;
